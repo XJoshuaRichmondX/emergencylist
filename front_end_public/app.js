993ca2bc1cbd4014376bec7b970f6a4bd777c32a
Greyes90 <greyes0731@gmail.com>
2022-10-06 18:03:50 -0500

    html, local and back server , and database complete

---
+++ front_end_public/app.js
@@ -0,0 +1,3 @@
+
+//utilize fetch and then function to retrieve data to output in the HTML
+
---
+++ front_end_public/localServer.js
@@ -0,0 +1,14 @@
+
+
+const app = express()
+const PORT = 3003
+const express = require('express')
+const cors = require('cors')
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`local server running on ${PORT}`)
+});
